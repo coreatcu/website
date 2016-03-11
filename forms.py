c64123cadb50ee4653668eec3a112a1504bfe145
Anshul Gupta <anshul.dlf@gmail.com>
2016-03-11 05:01:19 -0500

    adding captcha

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import Form
+from flask.ext.wtf.recaptcha import RecaptchaField
 from wtforms import TextField, TextAreaField
 from wtforms.validators import DataRequired, Email
-
 
 class ContactForm(Form):
     name = TextField('Name', validators=[DataRequired()])
     email = TextField('Email', validators=[DataRequired(), Email()])
     subject = TextField('Subject', validators=[DataRequired()])
     message = TextAreaField('Message', validators=[DataRequired()])
+    recaptcha = RecaptchaField()
